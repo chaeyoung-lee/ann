d4491f0e55f89539b2c4be297fa59b684c9e99a5
Chae Young Lee <cylee@hafs.hs.kr>
2018-10-31 11:23:10 +0900

    Update xor2.py

--- xor2.py
+++ xor2.py
@@ -1,15 +1,12 @@
-
 import numpy as np
 
 # Sigmoid Function
 def act(x):
     return 1/(1+np.exp(-x))
 
-
 # datasets
 x = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
 y = np.array([[0, 1, 1, 0]]).T     # xor
-
 
 # parameters
 input_size = x.shape[1] # original input + bias
@@ -17,11 +14,9 @@ hidden_size = 4
 output_size = 1
 alpha = 0.1    # learning rate
 
-
 # weights
 w1 = np.random.randn(input_size, hidden_size)
 w2 = np.random.randn(hidden_size, output_size)
-
 
 # training
 for iter in xrange(10000):
@@ -36,7 +31,6 @@ for iter in xrange(10000):
 	delta1 = np.dot(delta2, w2.T) * (a1 * (1-a1))
 	w2 -= alpha * np.dot(a1.T, delta2)
 	w1 -= alpha * np.dot(x.T, delta1)
-
 
 # test
 z1 = np.dot(x, w1)
